45956bb5478817eb47048e04405af2a58546c667
Aya Abdelkhalk <aya.abdelkhalk30@gmail.com>
2024-10-05 04:38:16 +0300

    Add Book

---
+++ Application/ViewModels/Publisher/ViewPublisherVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.Publisher
+{
+    public class ViewPublisherVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,47 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Application.ViewModels.Book;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository 
+    {
+        private readonly LibraryDbContext _libraryDbContext;
+        public BookRepository(LibraryDbContext context ):base(context)
+        {
+            _libraryDbContext = context;
+        }
+
+        public async Task AddBookAsync(AddBookVM bookVM)
+        {
+            var book=new Book{
+                Name= bookVM.Name,
+                Author=bookVM.Author,
+                Date=DateTime.UtcNow,
+                Description=bookVM.Description,
+                ISBN=bookVM.ISBN,
+                Number=bookVM.Number,
+                PublisherId=bookVM.PublisherId,
+                //price
+            };
+            // Add genres
+            foreach (var genreId in bookVM.GenreIds)
+            {
+                var genre = await _libraryDbContext.Genres.FindAsync(genreId);
+                if (genre != null)
+                {
+                    book.Genres.Add(genre);
+                }
+            }
+            await AddAsync(book);
+        }
+    }
+     
+}
