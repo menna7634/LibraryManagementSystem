b2856634265facf68585e0746640a56071f6cdd0
Menna Mohamed <mennamohammed178@gmail.com>
2024-10-14 14:02:19 +0300

    Solve Errors

--- LibraryManagementSystem/Controllers/MemberMangementController.cs
+++ LibraryManagementSystem/Controllers/MemberMangementController.cs
@@ -19,12 +19,12 @@ namespace LibraryManagementSystem.Controllers
             
            var result = await _userRepository.GetUsersPaginatedAsync(pageNumber, pageSize);
 
-            ViewData["searchUser"] = string.Empty; // Or set to a default value if needed
-            ViewData["searchEmail"] = string.Empty; // Set as needed
-            ViewData["searchJoinedAt"] = null; // Set as needed
-            ViewData["TotalUsers"] = result.TotalCount; // Assuming your result has TotalCount property
+            ViewData["searchUser"] = string.Empty; 
+            ViewData["searchEmail"] = string.Empty; 
+            ViewData["searchJoinedAt"] = null; 
+            ViewData["TotalUsers"] = result.TotalCount; 
 
-            return View("GetMembers",result); // Pass the result to the view
+            return View("GetMembers",result); 
         }
 
         // GET: MemberMangement/Search
@@ -32,7 +32,7 @@ namespace LibraryManagementSystem.Controllers
         public async Task<IActionResult> Search(string searchUser, string searchEmail, DateTime? searchJoinedAt, string orderBy = "UserName", int pageNumber = 1, int pageSize = 10)
         {
             var result = await _userRepository.SearchUsersAsync(searchUser, searchEmail, searchJoinedAt, orderBy, pageNumber, pageSize);
-            return View("GetMembers", result); // Reuse Index view for displaying search results
+            return View("GetMembers", result); 
         }
 
         // GET: MemberMangement/Details/{id}
@@ -50,7 +50,7 @@ namespace LibraryManagementSystem.Controllers
                 return NotFound();
             }
 
-            return View(user); // Pass the user to the details view
+            return View(user); 
         }
 
        
